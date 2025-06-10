bd25da23dc11ba98455ef102527b04e9007d513d
XiaoC <xiaochai.joe@gmail.com>
2025-06-10 10:47:57 -0700

    update simulations

--- script/atomInterferometry/aiPhaseFloquet.m
+++ script/atomInterferometry/aiPhaseFloquet.m
@@ -1,4 +1,4 @@
-function [phi,pop] = aiPhaseFloquet(ol,F,alpha,freq)
+function [phi,phiD,phiS,pop] = aiPhaseFloquet(ol,F,alpha,freq)
 %AIPHASEANALYTICAL Summary of this function goes here
 %   Detailed explanation goes here
 %% Parameters
