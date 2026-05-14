0ad3889bd7a3d14460226ba57766cb0daaeed5d0
XiaoC <xiaochai.joe@gmail.com>
2026-05-14 11:00:48 -0700

    updated simulation

--- script/kapitza/dualLatticeKdAnalysis.m
+++ script/kapitza/dualLatticeKdAnalysis.m
@@ -67,4 +67,40 @@ end
 %% Group data
 kp1AmpV = becExp.HardwareData.hw_KP1AmpV;
 kp2AmpV = becExp.HardwareData.hw_KP2AmpV;
-[kp1AmpV,kp2AmpV,depthFit2D] = computeAveErr2D(kp1AmpV,kp2AmpV,depthFit,"None");
+[kp1AmpV1D,kp2AmpV1D,depthFit2D] = computeAveErr2D(kp1AmpV,kp2AmpV,depthFit,"None");
+[kp1AmpV1D,kp2AmpV1D,pulseAmplitude1] = computeAveErr2D(kp1AmpV,kp2AmpV,pulseAmplitude{1},"None");
+[kp1AmpV1D,kp2AmpV1D,pulseAmplitude2] = computeAveErr2D(kp1AmpV,kp2AmpV,pulseAmplitude{2},"None");
+
+%% Plot
+imagesc(depthFit2D,XData = mean(pulseAmplitude1,1) * k1, YData = mean(pulseAmplitude2,2) * k2)
+xlabel("KP1 Depth [$E_{\mathrm{R}}$]")
+ylabel("KP2 Depth [$E_{\mathrm{R}}$]")
+cb = colorbar;
+cb.Label.String = "Total Depth [$E_{\mathrm{R}}$]";
+render
+hold on
+x = linspace(0,300,1000);
+plot(x,x,'Color','w','LineWidth',1.5,'LineStyle','--')
+
+%% Plot at y=x
+[X,Y] = meshgrid(mean(pulseAmplitude1,1) * k1,mean(pulseAmplitude2,2) * k2);
+interpPoints = linspace(3,120,1000);
+V0Total = interp2(X,Y,depthFit2D,interpPoints,interpPoints);
+plot(interpPoints,V0Total)
+xlabel("Single Lattice Depth [$E_{\mathrm{R}}$]")
+ylabel("Dual Lattice Depth [$E_{\mathrm{R}}$]")
+render
+
+%% plot error
+[interpPointsX,interpPointsY] = meshgrid(interpPoints,interpPoints);
+V0Total2 = interp2(X,Y,depthFit2D,interpPointsX,interpPointsY);
+V0Error = abs(V0Total2 - abs(interpPointsX - interpPointsY));
+imagesc(V0Error,XData = interpPoints, YData = interpPoints)
+clim([0,15])
+xlabel("KP1 Depth [$E_{\mathrm{R}}$]")
+ylabel("KP2 Depth [$E_{\mathrm{R}}$]")
+cb = colorbar;
+cb.Label.String = "Total Depth Error [$E_{\mathrm{R}}$]";
+render
+hold on
+plot(x,x,'Color',[1,1,1,0.5],'LineWidth',1.5,'LineStyle','--')
